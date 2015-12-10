81c3487f1db3179da160ac1a53229e03d391fbb1
David Durika <durika.d@gmail.com>
2015-12-10 16:28:23 +0100

    hompage: added screen

--- both/pages/HomePage.jsx
+++ both/pages/HomePage.jsx
@@ -13,6 +13,9 @@ HomePage = React.createClass({
           <h1>Infinite Todo</h1>
           <h3>Repeated tasks made easy</h3>
           <div className="btn btn-primary btn-lg m-t" onClick={this.handleCreateList}>Create todo list</div>
+          <h2>Track all you repeated tasks</h2>
+          <p>Create list of repeated tasks like: write new article, read the news, clear my inbox, workout, cleanup my workplace. And don't procrastinate some tasks in favor of others.</p>
+          <img className="img-responsive" src="/basic-screen.png" alt="Repeated Todo"/>
           <div className="m-t-lg">
             <TasksChart />
           </div>
